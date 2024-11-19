5caec280207b22b6866464caeee6d0aa878fa37b
astuciasnor <astuciasnor@gmail.com>
2024-11-19 16:37:00 -0300

    adicionando arquivos, criando pasta imagens e movendo arquivos

--- importacao-dados.R
+++ importacao-dados.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(readxl)
 
-dados <- read_excel("dados/tabela_geral_estados.xlsx", sheet = 2) |> 
+aquicultura <- read_excel("dados/tabela_geral_estados.xlsx", sheet = 2) |> 
   filter(Ano == 2023) # Filtra pelo ano desejado
 
-dados |> 
+aquicultura |> 
   ggplot(aes(x = Estado, y = `Producao(t)`, fill = Estado)) +
   geom_col(show.legend = F)
